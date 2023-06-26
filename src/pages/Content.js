4df358a1f103c75354ffd7c5f33e19e3f392d899
Zach Berger <zachhberger@gmail.com>
2023-06-26 18:13:54 -0400

    added Content page logic for routing between components

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,1 +1,22 @@
 // Main component for conditionally rendering different pages based on route.
+
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import About from './About';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
+
+function Content() {
+    return (
+        <Switch>
+            <Route exact path="/" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/resume" component={Resume} />
+            <Redirect to="/" />
+        </Switch>
+    );
+}
+
+export default Content;
